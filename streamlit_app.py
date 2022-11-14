0551b878f31bd2762fe71e0fac03b3a0f8a800a6
abhi1-12 <108932900+abhi1-12@users.noreply.github.com>
2022-11-14 12:42:20 +0530

    Update streamlit_app.py
    added emogies and Smoothie text

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit
 
 streamlit.title("My Parents new Healthy Dinner")
 streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 and Blueberry Oatmeal")
+streamlit.text("🥗Omega 3 and Blueberry Oatmeal")
 streamlit.text("Kale, Spinach and Rocket Smothie")
-streamlit.text("Hard Boiled Free Range Egg")
+streamlit.text("🐔Hard Boiled Free Range Egg")
+streamlit.header('🥑Avacado Toast ')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
