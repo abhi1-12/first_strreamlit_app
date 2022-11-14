9d6dc8e7238a9728139a3ebb1a2d20ce40be1252
abhi1-12 <108932900+abhi1-12@users.noreply.github.com>
2022-11-14 13:06:13 +0530

    Update streamlit_app.py
    added variable to store multi select line and display only selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,9 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #lets put a select list so that they can select the fruit
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 #print the dataframe
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
 
 
