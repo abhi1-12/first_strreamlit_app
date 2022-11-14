dfe7a506b424fd12a9312344f7ff319560227c04
abhi1-12 <108932900+abhi1-12@users.noreply.github.com>
2022-11-14 12:36:27 +0530

    Update streamlit_app.py
    Added Header Break-fast Menu
    Added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@
 import streamlit 
 
 streamlit.title("My Parents new Healthy Dinner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 and Blueberry Oatmeal")
+streamlit.text("Kale, Spinach and Rocket Smothie")
+streamlit.text("Hard Boiled Free Range Egg")
